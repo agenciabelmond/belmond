80bf624017ea8e3a1e80ea75142a6085f1e8be18
eilonrafael-sk8 <eilonrafael@gmail.com>
2026-05-05 01:50:00 -0300

    Update page.tsx

--- app/anuncio/page.tsx
+++ app/anuncio/page.tsx
@@ -4,8 +4,36 @@ import Image from "next/image"
 import { FaWhatsapp } from "react-icons/fa"
 import { motion } from "framer-motion"
 
-export default function Home() {
-  return (
+export default function Home() { import { useEffect } from "react"
+ export default function Home() {
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments)
+        }
+        if (!f._fbq) f._fbq = n
+        n.push = n
+        n.loaded = true
+        n.version = "2.0"
+        n.queue = []
+        t = b.createElement(e)
+        t.async = true
+        t.src = v
+        s = b.getElementsByTagName(e)[0]
+        s.parentNode.insertBefore(t, s)
+      })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js")
+
+      fbq("init", "2082969819016692")
+      fbq("track", "PageView")
+    }
+  }, [])
+
+  return ( return (
     <main className="min-h-screen bg-black text-white flex flex-col selection:bg-yellow-500/20">
       {/* Header */}
       <header className="sticky top-0 z-20 w-full border-b border-zinc-900 bg-black/80 backdrop-blur-md">
